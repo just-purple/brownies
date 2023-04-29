a7b22018bf594f43ce6924b3f3283989e4bc9aab
just-purple <viola-burchi@virgilio.it>
2023-04-29 18:32:35 +0200

    add storage functionality to snake

--- 11-snake-init/app/app.go
+++ 11-snake-init/app/app.go
@@ -2,13 +2,14 @@ package app
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 
 	"github.com/eiannone/keyboard"
 	"github.com/just-purple/brownies/11-snake-init/terminal"
 )
 
-const APPLE = "🍎"
+const APPLE = "@"
 const SNAKE_BODY = "█"
 
 type Pair struct {
@@ -16,20 +17,33 @@ type Pair struct {
 }
 
 type App struct {
-	PlayGround Pair
-	Apples     []Pair
-	Snake      Pair
-	Direction  Pair
-	Delay      time.Duration
+	PlayGround    Pair
+	Apple         *Pair
+	StartingSnake Pair
+	Direction     Pair
+	Delay         time.Duration
+	StartingLen   int
+
+	snake []*Pair
 }
 
 func (a *App) handleInput(input keyboard.Key) {
-	// TODO:
-	//	- right now this function reverts the direction
-	//	- edit the Direction based on the user input
-	// 	- ex: if input == keyboard.KeyArrowDown { ... }
-
-	a.Direction = Pair{X: a.Direction.X * -1, Y: a.Direction.Y * -1}
+	if input == keyboard.KeyArrowDown {
+		a.Direction = Pair{X: 0, Y: 1}
+		return
+	}
+	if input == keyboard.KeyArrowUp {
+		a.Direction = Pair{X: 0, Y: -1}
+		return
+	}
+	if input == keyboard.KeyArrowLeft {
+		a.Direction = Pair{X: -1, Y: 0}
+		return
+	}
+	if input == keyboard.KeyArrowRight {
+		a.Direction = Pair{X: 1, Y: 0}
+		return
+	}
 }
 
 func (a *App) draw() {
@@ -48,12 +62,22 @@ func (a *App) draw() {
 
 		// inside the frame
 		for x := 0; x < a.PlayGround.X; x++ {
-			if x == a.Snake.X && y == a.Snake.Y {
-				fmt.Print(SNAKE_BODY)
-				continue
+			found := false
+			for _, v := range a.snake {
+				if v.X == x && v.Y == y {
+					fmt.Print(SNAKE_BODY)
+					found = true
+					break
+				}
+			}
+			if a.Apple.X == x && a.Apple.Y == y {
+				fmt.Print(APPLE)
+				found = true
 			}
 
-			fmt.Print(" ")
+			if !found {
+				fmt.Print(" ")
+			}
 		}
 
 		// right border
@@ -69,22 +93,86 @@ func (a *App) draw() {
 	fmt.Println("┘")
 }
 
+func (a *App) isDead() bool {
+	for i := 1; i < len(a.snake); i++ {
+		if a.snake[0].X == a.snake[i].X && a.snake[0].Y == a.snake[i].Y {
+			return true
+		}
+	}
+	return false
+}
+
+func (a *App) isEating() bool {
+	if a.Apple == nil {
+		return false
+	}
+	if a.snake[0].X == a.Apple.X && a.snake[0].Y == a.Apple.Y {
+		return true
+	}
+	return false
+}
+
+func (a *App) placeApple() {
+	// array (slice di pair) con tutte le posizioni vuote (dove non c'è il serpente)
+	// E POI RANdom selezioni un indice dell'array e poi piazzi la mela lì
+	// in questo modo generi un numero random fra quelli nell'array e verso la fine quando il serpente è grande
+
+	var tmp []Pair
+	for y := 0; y < a.PlayGround.Y; y++ {
+		for x := 0; x < a.PlayGround.X; x++ {
+			for _, v := range a.snake {
+				if v.X != x && v.Y != y {
+					tmp = append(tmp, Pair{
+						X: x,
+						Y: y,
+					})
+				}
+			}
+		}
+	}
+	iRand := rand.Intn(len(tmp))
+	a.Apple = &tmp[iRand]
+}
+
 func (a *App) evaluate() {
 	// TODO:
 	// - move the snake
 	// - here there is a problem :)
-	a.Snake.X = (a.Snake.X + a.Direction.X) % a.PlayGround.X
-	a.Snake.Y = (a.Snake.Y + a.Direction.Y) % a.PlayGround.Y
+	// alla posizione viene sommata la direzione, per questo nella funzione handleinput per gestire la direzione data in input le x e le y sono 0, 1, -1 con riferimento all'asse cartesiano con origine nel vertice in alto a sinistra del campo
 
-	// TODO-THEN:
-	// - this is the place where you check things
-	// - is the snake dead?
-	// - is the snake eating?
-	// - where do I place the apple
+	// tmp := make([]*Pair, len(a.snake))
+	// tmp[0] = &Pair{
+	// 	X: (a.snake[0].X + a.Direction.X + a.PlayGround.X) % a.PlayGround.X,
+	// 	Y: (a.snake[0].Y + a.Direction.Y + a.PlayGround.Y) % a.PlayGround.Y,
+	// }
 
+	// for i := 0; i < len(tmp)-1; i++ {
+	// 	tmp[i+1] = a.snake[i]
+	// }
+	// a.snake = tmp
+
+	for i := len(a.snake) - 1; i > 0; i-- {
+		a.snake[i] = &Pair{
+			X: a.snake[i-1].X,
+			Y: a.snake[i-1].Y,
+		}
+	}
+
+	v := a.snake[0]
+	v.X = (v.X + a.Direction.X + a.PlayGround.X) % a.PlayGround.X
+	v.Y = (v.Y + a.Direction.Y + a.PlayGround.Y) % a.PlayGround.Y
 }
 
-func (a *App) Run() {
+func (a *App) Run() (int, error) {
+	a.snake = make([]*Pair, a.StartingLen)
+	for i := 0; i < a.StartingLen; i++ {
+		a.snake[i] = &Pair{
+			X: a.StartingSnake.X - a.Direction.X*i,
+			Y: a.StartingSnake.Y - a.Direction.Y*i,
+		}
+
+	}
+	a.placeApple()
 	terminal.HideCursor()
 	inputChannel := make(chan keyboard.Key)
 
@@ -104,21 +192,30 @@ func (a *App) Run() {
 		}
 	}(inputChannel)
 
-stdinloop:
 	for {
 		// select the first available channel
 		select {
 		case input, ok := <-inputChannel:
 			if !ok {
-				break stdinloop
+				terminal.ShowCursor()
+				return len(a.snake), nil //TODO errors.New("game stopped")
 			}
 			a.handleInput(input)
 
 		case <-time.After(a.Delay):
 			terminal.Clean()
 			a.evaluate()
+			if a.isDead() {
+				terminal.ShowCursor()
+				return len(a.snake), nil
+			}
+			if a.isEating() {
+				// il serpente cresce di un pezzettino
+				a.snake = append(a.snake, &Pair{})
+				// piazzo la mela
+				a.placeApple()
+			}
 			a.draw()
 		}
 	}
-	terminal.ShowCursor()
 }
--- 11-snake-init/main.go
+++ 11-snake-init/main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"time"
 
 	"github.com/just-purple/brownies/11-snake-init/app"
 )
+
+const JSON_PATH = "./score.json"
+
+type UserScore struct {
+	Name  string
+	Score int
+}
 
 func main() {
 
@@ -12,15 +22,89 @@ func main() {
 
 	// MAYBE: this could return a score :)
 	app := &app.App{
-		PlayGround: app.Pair{X: 30, Y: 10},
-		Snake:      app.Pair{X: 1, Y: 1},
-		Direction:  app.Pair{X: 1, Y: 0},
-		Delay:      100 * time.Millisecond,
+		PlayGround:    app.Pair{X: 20, Y: 6},
+		StartingSnake: app.Pair{X: 5, Y: 5},
+		Direction:     app.Pair{X: 1, Y: 0},
+		Delay:         150 * time.Millisecond,
+		StartingLen:   5,
 	}
 
-	app.Run()
+	score, err := app.Run()
+	if err != nil {
+		return
+	}
+	topScores, err := getTop(JSON_PATH)
+	if err != nil {
+		fmt.Println("errore:", err)
+	}
+
+	if len(topScores) < 10 || score > topScores[9].Score {
+		var name string
+		fmt.Println("what's your name?")
+		fmt.Scanln(&name)
+
+		new := UserScore{
+			Name:  name,
+			Score: score,
+		}
+
+		topScores = orderedAppend(topScores, new)
+		if len(topScores) > 10 {
+			topScores = topScores[:10]
+		}
+	}
+	fmt.Println(topScores)
+	err = setTop(topScores, JSON_PATH)
+	if err != nil {
+		fmt.Println("errore:", err)
+	}
 
 	// MAYBE: make the user add a name and store the top 10 scores somewhere :)
 
 	// TODO: press any key to restart
+}
+
+// funzione che restituisce la lista dei top scores contenuti nel file json
+func getTop(path string) ([]UserScore, error) {
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		return []UserScore{}, nil
+	}
+
+	result := []UserScore{}
+	err = json.Unmarshal(content, &result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+// funzione che inserisce fra i top scores un nuovo score,
+// dopodichè restituisce la lista con i top scores aggiornata
+func orderedAppend(topScores []UserScore, new UserScore) []UserScore {
+	if len(topScores) == 0 {
+		return []UserScore{new}
+	}
+	// aggiungi nel posto giusto e se la lunghezza supera 10 ritorna solo i primi 10
+	i := 0
+	for i = 0; i < len(topScores); i++ {
+		if new.Score > topScores[i].Score {
+			break
+		}
+	}
+	app := append(topScores[:i], new)
+	if i < len(topScores) {
+		app = append(app, topScores[i:]...)
+	}
+	return app
+}
+
+// funzione che data la lista con i top scores la scrive nel file json
+func setTop(topScores []UserScore, path string) error {
+	content, err := json.Marshal(topScores)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(path, content, 0644)
+	return err
 }
